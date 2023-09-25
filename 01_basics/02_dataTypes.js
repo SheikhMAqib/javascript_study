9c2f1f6948d893a98a10ed2faa1cb0b45cbfe7c7
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-09-25 18:21:21 +0000

    Data Types in JavaScripts

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3 + 3);  //  we are using nodejs , not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("Mohammad Aqib");
+
+let name = "Mohammad Aqib";
+let age = 31;
+let isLogggedIn = false;
+let State;
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique;
+
+// Object
+
+console.log(typeof "Mohammad Aqib"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefine
+
+// www.tc39.es/ecma262/#sec-intro
