471a2d5fc0adf8e6106b0bb1bd0b4786de48b271
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-09-26 16:14:06 +0000

    operations in javascript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,5 +43,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************operations******************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello";
+let str2 = " Sheikh_Mohammad_Aqib";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);  // 12
+// console.log(1 + "2");   // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(2 + ((2 * 5) % 3));  // 3
+
+// console.log(true); // true
+// console.log(+true); // 1
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter);
