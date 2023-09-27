a9e73fcf948d3f8ff6b8d3bcde33a89ba974bdfd
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-09-27 18:24:26 +0000

    moved file

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,31 @@
+//  Primitive  // Reference // Call by value
+
+// 7 types : String , Number , null ,Boolean , undefined , Symbol , Bigint
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const tempIn = null;
+let useEmail;
+
+const id = Symbol("123");
+const anotherID = Symbol("123");
+console.log(id === anotherID);
+
+const BigNumber = 565425445425454545n;
+
+// Reference (Non Primitive )
+
+// Array, Objects, Functions;
+
+const hero = [alibaba, aliexpress, darazpk];
+
+let myObj = {
+  name: "Ali",
+  age: 22,
+};
+
+const myFunc = function () {
+  console.log("Hello World");
+};
