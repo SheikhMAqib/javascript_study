33706b767338d7229940835874139ab6b3b46f78
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-09-27 19:03:57 +0000

    Stack , Heap in javaScript

--- 01_basics/datatypes_summery.js
+++ 01_basics/datatypes_summery.js
@@ -11,7 +11,7 @@ let useEmail;
 
 const id = Symbol("123");
 const anotherID = Symbol("123");
-console.log(id === anotherID);
+// console.log(id === anotherID);
 
 const BigNumber = 565425445425454545n;
 
@@ -19,7 +19,7 @@ const BigNumber = 565425445425454545n;
 
 // Array, Objects, Functions;
 
-const hero = [alibaba, aliexpress, darazpk];
+// const hero = [alibaba, aliexpress, darazpk];
 
 let myObj = {
   name: "Ali",
@@ -27,5 +27,42 @@ let myObj = {
 };
 
 const myFunc = function () {
-  console.log("Hello World");
+  // console.log("Hello World");
 };
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// --------------------------***** Memory *****--------------------------------
+
+// Stack(Primitive), variable, Copy;
+
+// Heap(Non - Primitive) Reference;
+
+// -----------------******STACK********----------------------
+
+let myYoutubename = "AkhlasinternationMart";
+
+let anothername = myYoutubename;
+
+anothername = "akhlas";
+
+console.log(anothername);
+console.log(myYoutubename);
+
+// -----------------*******HEAP*******--------------------
+
+let userOne = {
+  email: "xzy@gmail.com",
+  network: "zong",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "ajj8632@gmail.com";
+userTwo.network = "jazz";
+
+console.log(userOne.email);
+console.log(userOne.network);
+
+console.log(userTwo.email);
+console.log(userTwo.network);
