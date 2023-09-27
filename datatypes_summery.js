7a21ffe9759bb7189ad6316cbc02789373e0a7f9
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-09-27 16:45:52 +0000

    datatypes summery

---
+++ datatypes_summery.js
@@ -0,0 +1,20 @@
+//  Primitive  // Reference // Call by value
+
+// 7 types : String , Number , null ,Boolean , undefined , Symbol , Bigint
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const tempIn = null;
+let useEmail;
+
+const id = Symbol("123");
+const anotherID = Symbol("123");
+console.log(id === anotherID);
+
+const BigNumber = 565425445425454545n;
+
+// Reference (Non Primitive )
+
+// Array, Objects, Functions;
