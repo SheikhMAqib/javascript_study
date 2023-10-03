a99d01faadf2ea99e4619595955579e41a5efded
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-10-03 15:15:37 +0000

    scope in javascript

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,9 +5,46 @@ let a = 300;
 if (true) {
   let a = 10;
   const b = 20;
-  console.log("INNER :", a);
+  // console.log("INNER :", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// -----------************-----------
+
+function one() {
+  const userName = "Mohammad Aqib";
+  function two() {
+    const website = "youtube";
+    // console.log(userName);
+  }
+  // console.log(website);  // error
+  two();
+}
+// one();
+
+// -----------************-----------
+
+if (true) {
+  const username = "Munsif";
+  if (username === "Munsif") {
+    const website = " facebook";
+    // console.log(username + website);
+  }
+}
+
+// -----------************-----------
+
+console.log(addOne(5));
+function addOne(num) {
+  return num + 1;
+}
+
+// -----------************-----------
+
+// addTwo(5); // error
+const addTwo = function (num) {
+  return num + 1;
+};
