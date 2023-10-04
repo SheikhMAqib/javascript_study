bc8b740a89d4a1896ca53d19dddf199a8523826e
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-10-04 17:29:58 +0000

    if else in JavaScript

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -148,11 +148,40 @@ const temperature = 41;
 
 // -------------****************---------------------
 
-const score = 200;
+// const score = 200;
 
-if (score > 100) {
-  let power = "fly";
-  console.log(`user power ${power}`);
+// if (score > 100) {
+//   let power = "fly"; // try "var" keyword see no error found
+//   console.log(`user power ${power}`);
+// }
+
+// console.log(`user power ${power}`);
+
+// ----------*************--------------------
+
+// const balance = 1000;
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("less than 750");
+// } else {
+//   console.log("less than 1200");
+// }
+
+// -------------------**********-------------------
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard && 2 == 3) {
+  console.log("Allow to buy course");
 }
 
-console.log(`user power ${power}`);
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("user Logged In");
+}
