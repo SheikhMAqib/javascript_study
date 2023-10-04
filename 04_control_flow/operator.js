12fa7ce5467e749b2e3bfc4b34fa9a5dfeb302c8
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-10-04 18:49:38 +0000

    Nullish Coalescing Or Ternary Operator in JavaScript

---
+++ 04_control_flow/operator.js
@@ -0,0 +1,23 @@
+//  Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+
+// val1 = 5 ?? 10;
+
+// val1 = null ?? 10;
+
+// val1 = undefined ?? 15;
+
+// val1 = null ?? 10 ?? 20;
+
+// console.log(val1);
+
+// ------------************------------
+
+// Ternary Operator
+
+// condition ? true : false;
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log("less Than 80 ") : console.log("More Than 80");
