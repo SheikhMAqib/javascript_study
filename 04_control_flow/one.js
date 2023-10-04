250f9ce94a161b109f3ec44c26578647a623e331
Mohammad Aqib <35155765+SheikhMAqib@users.noreply.github.com>
2023-10-04 17:07:50 +0000

    if in javascript

---
+++ 04_control_flow/one.js
@@ -0,0 +1,158 @@
+// < LessThan;
+// > GreaterThan;
+// <= EqualOrGreaterThan
+// >= EqualOrLesserThan;
+// == Equal
+// === StrickEqual
+// != NotEqual
+// !== negative side check
+// && And
+// || Or
+// ! Not
+// () Parenthesis
+// [] Bracket
+// {} CurlyBracket
+// . Dot
+// , Comma
+// ? QuestionMark
+// : Colon
+// = AssignmentOperator
+// += AdditionAssignmentOperator
+// -= SubtractionAssignmentOperator
+// *= MultiplicationAssignmentOperator
+// /= DivisionAssignmentOperator
+// %= ModulusAssignmentOperator
+// <<= LeftShiftAssignmentOperator
+// >>= RightShiftAssignmentOperator
+// &= BitwiseAndAssignmentOperator
+// |= BitwiseOrAssignmentOperator
+// ^= BitwiseXorAssignmentOperator
+// ++ PreIncrementOperator
+// -- PreDecrementOperator
+// -> PointerMemberAccessOperator
+// :: ScopeResolutionOperator
+// @ AtSign
+// $ DollarSign
+// # HashTag
+// ~ Tilde
+// & ReferenceType
+// * PointerTo
+// + UnaryPlus
+// - UnaryMinus
+// sizeof SizeOfKeyword
+// typeof TypeOfKeyword
+// new NewConstraint
+// delete DeleteConstraint
+// throw ThrowExceptionStatement
+// return ReturnStatement
+// break BreakStatement
+// continue ContinueStatement
+// goto GotoStatement
+// if IfElseIfElseStatement
+// switch SwitchSection
+// while DoWhileStatement
+// for ForEachInLoop
+// foreach ForeachInLoop
+// using UsingAliasDirective
+// namespace NamespaceDeclaration
+// class StructUnionEnumTypeDeclaration
+// interface InterfaceDeclaration
+// delegate DelegateDeclaration
+// enum EnumDeclaration
+// struct ClassStructConstructorDestructorMethodPropertyFieldEventDelegateParameterIndexerSetAccessorNamespaceVariableBlockCommentExpressionStatementFunctionPointerInvocationReturnThrowYieldBreakContinue
+// struct StructDeclaration
+// record RecordDeclaration
+// event EventDeclaration
+// method MethodDeclaration
+// property PropertyDeclaration
+// indexer IndexerDeclaration
+// field FieldDeclaration
+// constructor ConstructorDeclaration
+// destructor DestructorDeclaration
+// operator OperatorOverloadableBinaryExpression
+// conversion ConversionOperatorDeclaration
+// static StaticConstructorDeclaration
+// get SetAccessorDeclaration
+// set GetAccessorDeclaration
+// add EventAddAccessorDeclaration
+// remove EventRemoveAccessorDeclaration
+// fixed FixedVariableDeclaration
+// lock LockStatement
+// try CatchClause FinallyBlock
+// checked CheckedUncheckedStatement
+// unchecked UncheckedStatement
+// unsafe UnsafeStatement
+// default DefaultValueExpression
+// nameof NameofExpression
+// arglist ArgListExpression
+// anonymousobject AnonymousObjectCreationExpression
+// objectcreation ObjectCreationExpression
+// arraycreation ArrayCreationExpression
+// elementaccess ElementAccessExpression
+// invocation InvocationExpression
+// memberbinding MemberBindingExpression
+// this ThisReferenceExpression
+// base BaseReferenceExpression
+// postincrement PostIncrementExpression
+// predecrement PreDecrementExpression
+// trueliteral TrueLiteralExpression
+// falseliteral FalseLiteralExpression
+// charliteral CharacterLiteralExpression
+// stringliteral StringLiteralExpression
+// integerliteral IntegerLiteralExpression
+// longintegerliteral LongIntegerLiteralExpression
+// ulongintegerliteral ULongIntegerLiteralExpression
+// floatingpointliteral FloatingPointLiteralExpression
+// nullliteral NullLiteralExpression
+// pseudonullliteral PseudoNullLiteralExpression
+// emptyinitializer EmptyInitializerExpression
+// initializerarray InitializerArrayExpression
+// initializertuple InitializerTupleExpression
+// queryexpression QueryExpression
+// fromclause FromClause
+// letclause LetClause
+// whereclause WhereClause
+// orderbyclause OrderByClause
+// ascendingdescendingordering AscendingDescendingOrdering
+// join JoinClause
+// groupgroupbyclause GroupByClause
+// intointoclause IntoClause
+// selectselectclause SelectClause
+// onintoclause OnIntoClause
+// equalsvalueclause EqualsValueClause
+// argument Argument
+// expression Expression
+
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// if (temperature < 50) {
+//   console.log("less than 50"); // It's warm outside!
+// }
+// console.log("temperature is greater than 50");
+// ----------------------***************--------------------
+// if (temperature === 41) {
+//   console.log("less than 50"); // It's warm outside!
+// } else {
+//   console.log("temperature is greater than 50");
+// }
+// console.log("execute");
+
+// ---------------************-----------------------
+// if (temperature === 40) {
+//   console.log("less than 50"); // It's warm outside!
+// } else {
+//   console.log("temperature is greater than 50");
+// }
+// console.log("execute");
+
+// -------------****************---------------------
+
+const score = 200;
+
+if (score > 100) {
+  let power = "fly";
+  console.log(`user power ${power}`);
+}
+
+console.log(`user power ${power}`);
